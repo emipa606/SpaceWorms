422ab1241c15f92fc6f700861327fd8e0737e6d3
Mlie <emipa606@users.noreply.github.com>
2020-05-22 19:35:51 +0200

    First publish

---
+++ Source/Scuttlebugs/QueenWorm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Verse;
+using Verse.AI.Group;
+
+namespace Scuttlebugs
+{
+    public class QueenWorm : ThingWithComps
+    {
+
+    }
+
+}
